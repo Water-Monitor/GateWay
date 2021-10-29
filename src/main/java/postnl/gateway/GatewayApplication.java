ae40488b463e3d67341e565052fe30301a3e6360
tjalk20sd <tjalk20sd@gmail.com>
2021-10-29 15:24:19 +0200

    added working gateway

---
+++ src/main/java/postnl/gateway/GatewayApplication.java
@@ -0,0 +1,18 @@
+package postnl.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.server.adapter.ForwardedHeaderTransformer;
+
+@SpringBootApplication
+public class GatewayApplication {
+	@Bean
+	public ForwardedHeaderTransformer forwardedHeaderTransformer() {
+		return new ForwardedHeaderTransformer();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+}
